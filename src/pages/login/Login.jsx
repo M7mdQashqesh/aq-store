2438b4bf44cb9208c52da3948c195158fa7e534d
Mohammed Qashqesh <mohammedbelal601@gmail.com>
2025-02-10 17:09:50 +0200

    remove data from console

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,12 +2,6 @@ import LoginForm from "../../components/loginForm/LoginForm";
 import "./login.css";
 
 const Login = () => {
-  const storedEmail = import.meta.env.VITE_EMAIL;
-  const storedPass = import.meta.env.VITE_PASSWORD;
-
-  console.log(storedEmail);
-  console.log(storedPass);
-
   return (
     <div className="login-page">
       <div className="image">
